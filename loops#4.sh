d0015adb4836af6b0eb08149d741fa61115fb006
a.wagner <wagner.agnieszka@gmail.com>
2018-08-24 16:28:46 +0200

    changes in the original directory

--- loops#4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-until [ "$n" = end ]
-do
-	echo "provide some input or write END to exit"
-	read n
-done
--- loops.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-colors1="blue-brown-black"
-colors2="blue+brown+black"
-echo "IFS=-"
-IFS=-
-echo $colors1
